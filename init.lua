5cb31041f587b412dae9a3d98ec7350c99598f42
vz443 <vz443@proton.me>
2026-09-07 23:00:35 +1000

    further clangd

--- init.lua
+++ init.lua
@@ -2,10 +2,34 @@ vim.pack.add{
   { src = 'https://github.com/neovim/nvim-lspconfig' },
 }
 
- -- `nvim-lspconfig` plugin. See |:h vim.lsp.Config| for all
- local clangd_opts = {}
+
+-- vim configs
+
+vim.diagnostic.config({
+  virtual_text = true,      -- Inline error messages
+  signs = true,             -- Gutter symbols
+  underline = true,         -- Underline problem code
+  update_in_insert = false, -- Don't shift text while typing
+  severity_sort = true,
+})
+
+
+-- clangd
+
+ local clangd_opts = {
+	 cmd = { 'clangd', '--background-index'}
+ }
 
 
  if not vim.lsp.is_enabled('clangd') then
      vim.lsp.enable('clangd', clangd_opts)
  end
+
+ -- md renderer
+vim.pack.add({
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
+    -- 'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
+    -- 'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+})
